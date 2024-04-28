298f00b65278528da987a4a5b307d97cb9b68b7c
ibbya04 <ibraheem.m.a04@gmail.com>
2024-04-28 17:31:40 +0100

    Update socialNetwork.java

--- socialNetwork.java
+++ socialNetwork.java
@@ -3,17 +3,22 @@ public class socialNetwork {
     private Graph graph;
     private inputReader ir;
 
+    // constructor to combine and run all functions
     public socialNetwork() {
         String filePath = "test-socialnetworks/social-network1.txt";
 
         this.createGraph(filePath);
+
+        //testing
         graph.printGraph();
     }
 
-    //
+    // creates a graph by calling readLine function to get line of text
+    // then createPerson function creates nodes & edges for that line of people/ 
     public void createGraph(String filePath) {
         ir = new inputReader();
         graph = new Graph();
+
         String[] line = ir.readLine(filePath);
         graph.createPerson(line);
     }
